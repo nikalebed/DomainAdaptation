f8b5491a522e954e9be961ecb16a46bee0442f5b
Veronika Lebedyuk <veronika.lebedyuk@gmail.com>
2024-03-28 15:32:31 +0300

    add seed

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from pprint import pprint
 from omegaconf import OmegaConf
 from utils.args import load_config
 from trainer import DomainAdaptationTrainer
+import random
+import torch
+import numpy as np
 
 config = load_config()
 pprint(OmegaConf.to_container(config))
+
+seed = config.exp.seed
+
+random.seed(seed)
+torch.random.manual_seed(seed)
+np.random.seed(seed)
 
 trainer = DomainAdaptationTrainer(config)
 trainer.setup()
