c3eb43910031ca8e900c52abf00856b2d4a06479
Veronika Lebedyuk <veronika.lebedyuk@gmail.com>
2024-04-02 21:56:13 +0300

    modify evaluation and visualisation

--- evaluate.py
+++ evaluate.py
@@ -1,43 +1,69 @@
+import clip
+from utils.common import load_clip
+import torch.nn.functional as F
 import os
 import click
 from utils.sg2_utils import Inferencer
 import torch
 from omegaconf import OmegaConf
-from utils.image_utils import get_image_t, construct_image_grid, resize_batch
-from PIL import Image
 from core import lpips
-from torchmetrics.multimodal.clip_score import CLIPScore
 from pathlib import Path
 from pprint import pprint
+from utils.image_utils import resize_batch
+from tqdm import tqdm
 
 
 class Evaluator:
-    def __init__(self, device='cuda'):
+    def __init__(self, device='cuda', n_samples=1000, batch_size=16, seed=0):
         self.device = device
         self.lpips = lpips.PerceptualLoss(model="net-lin", net="vgg", use_gpu=True)
-        self.clip_score = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
+        self.clip_model, self.clip_preprocess = load_clip('ViT-B/16', device)
+        self.n_samples = n_samples
+        self.batch_size = batch_size
+        self.seed = seed
 
-    def calc_metrics(self, src, trg, style):
-        src = resize_batch(src, 256).to(self.device)
-        trg = resize_batch(trg, 256).to(self.device)
+    def encode_image(self, image, norm=True):
+        image_features = self.clip_model.encode_image(self.clip_preprocess(image)).detach()
+        if norm:
+            image_features /= image_features.clone().norm(dim=-1, keepdim=True)
+        return image_features
+
+    def encode_text(self, text, templates=("{}",)):
+        text = [t.format(text) for t in templates]
+        tokens = clip.tokenize(text).to(self.device)
+        text_features = self.clip_model.encode_text(tokens).detach().float()
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        return text_features
+
+    def clip_score(self, imgs, text):
+        imgs = imgs.to(self.device)
+        imgs_encoded = self.encode_image(imgs)
+        text_encoded = self.encode_text(text)
+        return F.cosine_similarity(imgs_encoded, text_encoded)
+
+    def calc_metrics(self, net):
+        style_path = net.config.training.target_class
+        style = Path(style_path).stem
+
+        torch.manual_seed(self.seed)
+
+        lpips = 0
+        cs = 0
+
+        for i in tqdm(range((self.n_samples + self.batch_size - 1) // self.batch_size)):
+            zs = [torch.randn(min(self.batch_size, self.n_samples - i * self.batch_size), 512, device=self.device)]
+            src, trg = net(zs)
+
+            lpips += self.lpips(resize_batch(src, 256), resize_batch(trg, 256)).sum().item()
+            cs += self.clip_score(trg, style).sum().item()
+
         return {
-            'LPIPS': self.lpips(src, trg),
-            'CLIPScore': self.clip_score(src, [style] * src.size(0))
+            'LPIPS': lpips / self.n_samples,
+            'CLIPScore': cs / self.n_samples
         }
 
 
 DEFAULT_CONFIG_DIR = 'configs'
-DEFAULT_IMAGE_DIR = 'images'
-
-
-def get_latents(dir, n_examples=None):
-    latents = []
-    assert os.path.isdir(dir), '%s is not a valid directory' % dir
-    for root, _, fnames in sorted(os.walk(dir)):
-        for fname in sorted(fnames)[:n_examples]:
-            path = os.path.join(root, fname)
-            latents.append(torch.load(path))
-    return torch.cat(latents)
 
 
 @click.command()
@@ -47,29 +73,12 @@ def main(config_name):
 
     config_path = os.path.join(DEFAULT_CONFIG_DIR, config_name)
     config = OmegaConf.load(config_path)
-    latents = get_latents(config.latents_dir, config.n_examples)
-    latents = latents.to('cuda')
 
-    styles = []
-    src = None
-    rows = []
-
-    pprint(config)
     metrics = {}
 
     for ckpt in config.ckpts:
         net = Inferencer((os.path.join(config.checkpoints_dir, ckpt)))
-        style_path = net.config.training.target_class
-
-        styles.append(get_image_t(style_path))
-        src, trg = net([latents], input_is_latent=True)
-        rows.append(trg)
-
-        metrics[net.config.training.target_class] = eval.calc_metrics(src, trg, Path(style_path).stem)
-
-    arr = construct_image_grid(header=src, index=styles, imgs_t=rows, size=config.img_size)
-    img = Image.fromarray(arr.astype('uint8'), 'RGB')
-    img.save(os.path.join(DEFAULT_IMAGE_DIR, config.res_name))
+        metrics[net.config.training.target_class] = eval.calc_metrics(net)
 
     pprint(metrics)
 
---
+++ visualize.py
@@ -0,0 +1,47 @@
+import os
+import click
+from utils.sg2_utils import Inferencer
+import torch
+from omegaconf import OmegaConf
+from utils.image_utils import get_image_t, construct_image_grid
+from PIL import Image
+
+DEFAULT_CONFIG_DIR = 'configs'
+DEFAULT_IMAGE_DIR = 'images'
+
+def get_latents(dir, n_examples=None):
+    latents = []
+    assert os.path.isdir(dir), '%s is not a valid directory' % dir
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in sorted(fnames)[:n_examples]:
+            path = os.path.join(root, fname)
+            latents.append(torch.load(path))
+    return torch.cat(latents)
+
+
+@click.command()
+@click.argument('config_name', default='eval.yaml')
+def main(config_name):
+    config_path = os.path.join(DEFAULT_CONFIG_DIR, config_name)
+    config = OmegaConf.load(config_path)
+    latents = get_latents(config.latents_dir, config.n_examples)
+    latents = latents.to('cuda')
+
+    styles = []
+    src = None
+    rows = []
+    for ckpt in config.ckpts:
+        net = Inferencer((os.path.join(config.checkpoints_dir, ckpt)))
+        style_path = net.config.training.target_class
+
+        styles.append(get_image_t(style_path))
+        src, trg = net([latents], input_is_latent=True)
+        rows.append(trg)
+
+    arr = construct_image_grid(header=src, index=styles, imgs_t=rows, size=config.img_size)
+    img = Image.fromarray(arr.astype('uint8'), 'RGB')
+    img.save(os.path.join(DEFAULT_IMAGE_DIR, config.res_name))
+
+
+if __name__ == '__main__':
+    main()
